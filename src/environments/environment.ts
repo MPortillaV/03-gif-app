83bf1e30b1d7424df526b4d5532f78d514d328a7
Marcelo Portilla <portilla.villalobos@gmail.com>
2025-09-09 23:58:59 -0300

    se agrega el projecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'URkDrg6jZKAhmi2c7FtGUuj0P9hBYpbN',
+  giphyUrl: 'https://api.giphy.com/v1',
+
+
+};
